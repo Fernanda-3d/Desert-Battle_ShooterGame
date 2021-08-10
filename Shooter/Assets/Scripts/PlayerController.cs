4cd1e2e44096b826269deae910328cc057373126
Fernanda-3d <81561132+Fernanda-3d@users.noreply.github.com>
2021-08-10 20:26:24 +0100

    10-08-2021

--- Shooter/Assets/Scripts/PlayerController.cs
+++ Shooter/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,19 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] InputAction movement; //NEW
+    [SerializeField] InputAction fire; //NEW
+
     [SerializeField] float speed = 10f;
     [SerializeField] float xRange = 12f;
     [SerializeField] float yRange = 8f;
 
+    [SerializeField] GameObject[] lasers;
+
     [SerializeField] float positionPicthFactor = -2f;
     [SerializeField] float controlPitchFactor = -10f;
+
+    [SerializeField] float positionYawFactor = -1f;
+    [SerializeField] float controlRollFactor = -15f;
         
 
     float xMove;
@@ -28,11 +35,13 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         movement.Enable();
+        fire.Enable();
     }
 
     private void OnDisable() 
     {
         movement.Disable();
+        fire.Disable();
     }
 
     // Update is called once per frame
@@ -40,13 +49,13 @@ public class PlayerController : MonoBehaviour
     {
       Move();
       Rotate();
-
+      Fire();
     }
 
  void Move()
- {
+ { 
    // NEW INPUT SYSTEM
-    xMove = movement.ReadValue<Vector2>().x;
+    xMove = movement.ReadValue<Vector2>().x; //this movement is the variable created  as InputAction
     yMove = movement.ReadValue<Vector2>().y;
 
     float xOffset = xMove * Time.deltaTime * speed; //My moving position = to my input, so them is changed as much as I press * the velocity I want to
@@ -61,16 +70,14 @@ public class PlayerController : MonoBehaviour
 
     //mathf.Clamp() is used to constrain movement - don't go more than we want to the player go. We'll need to crate min
     //and max variables, can create a Range as well
-
-    
-    
-
+     
         // OLD INPUT SYSTEM 
        /* float horizontalMove = Input.GetAxis ("Horizontal");
         Debug.Log(horizontalMove);
 
         float verticalMove = Input.GetAxis ("Vertical");
         Debug.Log(verticalMove); */
+      
  }
 
  void Rotate()
@@ -81,12 +88,51 @@ public class PlayerController : MonoBehaviour
      float pitch = pitchPosition + picthControl; // SENTENCE1 |  this is the same as the sentece bellow but organized with the floats up,
                                                  //just to make it more readable
      //float pitch =  transform.localPosition.y * positionPicthFactor + yMove * controlPitchFactor;  //SENTENCE2
-
-     float yaw =  0f;
-     float roll =  0f;
-                                                  //Rool(Z), Pitch(X) and Yaw(Y) - 
+     float yaw = transform.localPosition.x * positionYawFactor;    
+     float roll = xMove * controlRollFactor;
+                                                                    //Pitch(X), Yaw(Y) Rool(Z)  - 
      transform.localRotation = Quaternion.Euler(pitch, yaw, roll); //Quaternion.Euler(x, y, z)** this is the same as (picth, yall, roll)
 
+ }
+
+ void Fire()
+ {
+     //OLD INPIT SYSTEM  - uses bool
+     /*
+     if (Input.GetButton("Fire1")) -- this "Fire1" is already set up in the project manager
+     {
+        Debug.Log("I'm shooting");        
+     }
+     else
+     {
+         Debug.Log("I'm not shooting"); 
+     }
+     */
+
+     //NEW INPIT SYSTEM  - uses float
+
+     if(fire.ReadValue<float>() > 0.5)
+     {
+        Debug.Log("I'm shooting");
+        LaserOn();
+     }
+     else
+     {
+         LaserOff();
+         Debug.Log("I'm not shooting"); 
+     }
+
+     void LaserOn()
+     {
+         //for each of the lasers that we have, turn them on
+     }
+
+     void LaserOff()
+     {
+         //for each of the lasers that we have, turn them off
+     }
+
+     
  }
 
 
